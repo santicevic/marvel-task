3cf5202ec16bc6a4d7e5b73332e43a9715205525
santicevic <sergijeanticevic@gmail.com>
2020-08-10 19:38:47 +0200

    Set up basic FE api endpoints

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import React from 'react';
 import './App.css';
+import dotenv from 'dotenv';
+import React, { useState, useEffect } from 'react';
+import { getCharactersByName } from './api/index'
+
+dotenv.config();
 
 function App() {
+  const [nameSubstring, setNameSubstring] = useState('');
+  const [characters, setCharacters] = useState([]);
+  useEffect(() => { 
+    nameSubstring && getCharactersByName(nameSubstring)
+      .then(({ results }) => setCharacters(results))
+  }, [nameSubstring])
+
   return (
     <div className="App">
-      Hello
+      <input
+        value={nameSubstring}
+        onChange={e => setNameSubstring(e.target.value)} />
+      { characters.map(character => (
+        <div key={ character.id }>
+          <h3>{character.name}</h3>
+          <h5>{character.description}</h5>
+        </div>
+      )) }
     </div>
   );
 }
---
+++ src/api/index.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import md5 from 'md5';
+
+const base = 'https://gateway.marvel.com/v1/public';
+
+const apiKey = process.env.REACT_APP_MARVEL_PUBLIC_KEY;
+const privateKey = process.env.REACT_APP_MARVEL_PRIVATE_KEY;
+const ts = new Date().getTime();
+
+export async function getCharactersByName(nameSubstring){
+   const { data: response } = await axios.get(`${base}/characters`, {
+        params: { 
+            apikey: apiKey,
+            hash: md5(ts+privateKey+apiKey),
+            ts,
+            nameStartsWith: nameSubstring && nameSubstring
+        }
+    })
+    return response.data;
+}
