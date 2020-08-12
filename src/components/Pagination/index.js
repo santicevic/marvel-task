eac8210cabf26f9cfab15066330821f1d0092166
santicevic <sergijeanticevic@gmail.com>
2020-08-13 00:14:03 +0200

    Added pagination css

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -1,14 +1,26 @@
-import React from "react";
+import "./styles.css";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 
 const Pagination = () => {
+  const [currentPage, setCurrentPage] = useState(0);
   const totalPages = useSelector(state =>
     Math.ceil(state.characters.total / state.characters.limit)
   );
   return (
-    <div>
-      {!isNaN(totalPages) &&
-        [...Array(totalPages)].map((a, i) => <span key={i}>{i + 1}</span>)}
+    <div className="pagination-wrapper">
+      <div className="pagination-container">
+        {!isNaN(totalPages) &&
+          [...Array(totalPages)].map((a, i) => (
+            <span
+              key={i}
+              className={currentPage === i ? "pagination-active" : null}
+              onClick={() => setCurrentPage(i)}
+            >
+              {i + 1}
+            </span>
+          ))}
+      </div>
     </div>
   );
 };
